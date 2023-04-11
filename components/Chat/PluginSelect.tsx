1272a9c074d13b7929fa9f7a1daca2c9b76a484e
lsx5223 <130224103+lsx5223@users.noreply.github.com>
2023-04-11 11:20:55 +0800

    Update PluginSelect.tsx

--- components/Chat/PluginSelect.tsx
+++ components/Chat/PluginSelect.tsx
@@ -18,5 +18,25 @@ export const PluginSelect: FC<Props> = ({ plugin, onPluginChange }) => {
     }
   }, []);
 
-
+  return (
+    <div className="flex flex-col">
+      <div className="w-full rounded-lg border border-neutral-200 bg-transparent pr-2 text-neutral-900 dark:border-neutral-600 dark:text-white">
+       <select
+          ref={selectRef}
+          className="w-full cursor-pointer bg-transparent p-2"
+          placeholder={t('Select a plugin') || ''}
+          value={plugin?.id || ''}
+          onChange={(e) => {
+            onPluginChange(
+              PluginList.find(
+                (plugin) => plugin.id === e.target.value,
+              ) as Plugin,
+            );
+          }}
+        >
+         
+        </select> 
+      </div>
+    </div>
+  );
 };
